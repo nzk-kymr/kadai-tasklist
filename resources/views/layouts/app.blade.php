e5a698bd2c3f767d7e6cbba771d6e6e27a693931
kymr.nzk <kymrxnzk@gmail.com>
2019-12-26 03:01:30 +0000

    validation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,6 +28,7 @@
        @include('commons.navbar')
         
         <div class="container">
+            @include('commons.error_messages')
             @yield('content')
         </div>
         
