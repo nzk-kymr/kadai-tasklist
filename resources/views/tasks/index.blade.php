f30ffbec12ee0c02c7501ffb6a4c9d9ce487e1a3
kymr.nzk <kymrxnzk@gmail.com>
2019-12-30 14:00:51 +0000

    user login

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -2,6 +2,10 @@
 
 @section('content')
 
+    @if (Auth::check())
+        {{ Auth::user()->name }}
+    @else
+
     <div class="center jumbotron">
         <div class="text-center">
 
@@ -9,6 +13,8 @@
             {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
         </div>
     </div>
+    
+    @endif
 
     @if (count($tasks) > 0)
         <table class="table table-striped">
